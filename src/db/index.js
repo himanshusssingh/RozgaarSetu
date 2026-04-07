93bcc2dc1b408ab03ed48b7f3a6606d30e5944ae
himanshusssingh <himanshusingh39397@gmail.com>
2026-04-07 23:53:12 +0530

    add cloud db

--- src/db/index.js
+++ src/db/index.js
@@ -1,11 +1,15 @@
 import mongoose, { connect } from "mongoose";
 import { dbName } from "../constant.js";
 
+const DBURI =
+  `${process.env.MONGODB_URI}` || `mongodb://127.0.0.1:27017/${dbName}`;
 
 const connectDB = async () => {
   try {
-    const connectionDB = await mongoose.connect(`${process.env.MONGODB_URI}/${dbName}`);
-    console.log(`Database connected successfully. || DB host: ${connectionDB.connection.host}`);
+    const connectionDB = await mongoose.connect(DBURI);
+    console.log(
+      `Database connected successfully. || DB host: ${connectionDB.connection.host}`,
+    );
   } catch (err) {
     console.log("Database Connection error: ", err);
     process.exit(1);
