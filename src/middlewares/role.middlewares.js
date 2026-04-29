d271d6188c6eb5e17af46b425da34358cbc4eea3
himanshusssingh <himanshusingh39397@gmail.com>
2026-04-29 16:44:51 +0530

    add middlewares for verify user role

---
+++ src/middlewares/role.middlewares.js
@@ -0,0 +1,63 @@
+import { asyncHandler } from "../utils/asyncHandler";
+import { ApiError } from "../utils/apiError";
+import { User } from "../models/user.models";
+
+const isUser = asyncHandler(async (req, res, next) => {
+  const role = req.user.role;
+
+  if (!role) {
+    req.flash("error", "Login to access this resource");
+    return res.redirect("/users/login");
+    throw new ApiError(403, "Forbidden, No role assigned.");
+  }
+
+  try {
+    if (role === "user") {
+      return next();
+    } else {
+      req.flash("error", "You don't have permission to access this resource");
+      return res.redirect("/home");
+      throw new ApiError(
+        403,
+        "Forbidden, You don't have permission to access this resource.",
+      );
+    }
+  } catch (err) {
+    throw new ApiError(
+      403,
+      err.message ||
+        "Forbidden, You don't have permission to access this resource.",
+    );
+  }
+});
+
+const isRecruiter = asyncHandler(async (req, res, next) => {
+  const role = req.user.role;
+
+  if (!role) {
+    req.flash("error", "Login to access this resource");
+    return res.redirect("/users/login");
+    throw new ApiError(403, "Forbidden, No role assigned.");
+  }
+
+  try {
+    if (role === "recruiter") {
+      return next();
+    } else {
+      req.flash("error", "You don't have permission to access this resource");
+      return res.redirect("/home");
+      throw new ApiError(
+        403,
+        "Forbidden, You don't have permission to access this resource.",
+      );
+    }
+  } catch (err) {
+    throw new ApiError(
+      403,
+      err.message ||
+        "Forbidden, You don't have permission to access this resource.",
+    );
+  }
+});
+
+export { isUser, isRecruiter };
